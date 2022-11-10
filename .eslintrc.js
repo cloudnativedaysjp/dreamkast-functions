a6a4d36afc836598494fd96ba753ea25b7565898
Hiroki Okui <hiroki.okui@gmail.com>
2022-11-11 02:07:30 +0900

    refactor: Add prettier and eslint packages to dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-var-requires': 'off',
+    'prettier/prettier': [
+      'error',
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      JSON.parse(require('fs').readFileSync('.prettierrc', 'utf8')),
+    ],
+  },
+}
